809d6fc922cec095044334bdb343b5d1f25f56f3
Mani Heshmati <maniheshmati1385@gmail.com>
2025-08-30 14:58:11 +0330

    fix navigation id

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -51,7 +51,7 @@
     </div>
 </nav>
 
-<aside id="default-sidebar" class="fixed top-0 right-0 z-40 w-64 h-screen transition-transform translate-x-full sm:translate-x-0" aria-label="Sidebar" >
+<aside id="logo-sidebar" class="fixed top-0 right-0 z-40 w-64 h-screen transition-transform translate-x-full sm:translate-x-0" aria-label="Sidebar" >
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             <li>
