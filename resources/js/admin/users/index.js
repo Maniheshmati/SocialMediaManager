dccd663f2de80336767a9d02da93c161e2c91cc1
Mani Heshmati <maniheshmati1385@gmail.com>
2025-09-05 01:01:34 +0330

    remove checkbox from tables for now

--- resources/js/admin/users/index.js
+++ resources/js/admin/users/index.js
@@ -38,15 +38,15 @@ document.addEventListener("DOMContentLoaded", () => {
         users.forEach(user => {
             const row = `
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="w-4 p-4">
-                        <div class="flex items-center">
-                            <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm">
-                        </div>
-                    </td>
+<!--                    <td class="w-4 p-4">-->
+<!--                        <div class="flex items-center">-->
+<!--                            <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm">-->
+<!--                        </div>-->
+<!--                    </td>-->
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${user.id}</th>
                     <td class="px-6 py-4">${user.name}</td>
                     <td class="px-6 py-4">${user.roles.length > 0 ? user.roles[0].name : 'No Role'}</td>
-                    <td class="px-6 py-4">${user.email}</td>
+                    <td class="px-6 py-4">${user.mobile}</td>
                     <td class="px-6 py-4 flex gap-2">
                         <a href="#"
                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline edit-user-btn"
