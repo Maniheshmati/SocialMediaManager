6a99e1f8650fcaa1103c290d176bc1d2afde68f8
Mani Heshmati <maniheshmati1385@gmail.com>
2025-09-05 08:34:58 +0330

    added profile_picture column for users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name')->nullable()->comment("it's something like Nickname");
             $table->string('mobile', 11)->unique();
             $table->string('email')->nullable()->unique();
+            $table->string('profile_picture')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
